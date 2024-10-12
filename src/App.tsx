63d879f2e792e6fac8e9e2483ccc98ed6fcdd0b1
T4nGe4nT <nichholcomb@gmail.com>
2024-10-12 17:37:27 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+// Component Imports
+
+
+// CSS Imports
+import './App.css'
+
+//Full Scoped variables
+
+
+
+function App() {
+
+
+  return (
+    <>
+        <p>Coming Soon!</p>
+
+    </>
+  )
+}
+
+export default App

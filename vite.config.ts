f8dc4f095b62ee60ab1ddee9dbbd295a4a6f1687
T4nGe4nT <nichholcomb@gmail.com>
2024-10-16 08:46:09 -0400

    Updates to index.html & vite config to finish deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   build: {outDir:"./build"},
   plugins: [react()],
+  base: "/portfoliov3/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
